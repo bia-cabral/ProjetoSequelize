1cc51e651111c3b18e9a172d9c67b224bb1a9020
Bia-Cabral <124395943+bia-cabral@users.noreply.github.com>
2025-10-01 06:58:24 -0300

    Add files via upload

---
+++ banco_instrucoes.sql
@@ -0,0 +1,10 @@
+-- Banco em PostgreSQL
+-- Comando utilizado para criação
+
+CREATE DATABASE nodesequelize
+    WITH 
+    OWNER = seu_usuario
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'pt_BR.UTF-8'
+    LC_CTYPE = 'pt_BR.UTF-8'
+    TEMPLATE = template0;
